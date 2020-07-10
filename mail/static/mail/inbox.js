cb6f4bb2a05cd2627471111249a0b1864872ab5e
Kean <kean.hopper@outlook.com>
2020-07-10 19:36:31 +0100

    Mailbox views improved
    Inbox, sent and archived emails now display in their mailboxes. Background color of read emails now changes.
    big cleanup of functions.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -11,11 +11,12 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function compose_email() {
+  document.querySelector('#temp').innerHTML = '';
+  document.querySelector('#arch').innerHTML = '';
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-  
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -24,16 +25,9 @@ function compose_email() {
   document.querySelector('#compose').onclick = function() {debug() } ;
 
   //Wait for compose-form button to be clicked, get the data from them, move them into variables for send_email func
-  //these are no longer running
-  //document.addEventListener('DOMContentLoaded', function() {
-  //document.querySelector('#compose-form').onsubmit = get_data;
+ 
   document.querySelector('#compose-form').onsubmit = function () {
-    /*const newRecipient = document.querySelector('#compose-recipients').value;
-    const newSubject = document.querySelector('#compose-subject').value;
-    const newBody = document.querySelector('#compose-body').value;*/
     get_data();
-    //console.log(`hello ${newRecipient} ${newSubject} ${newBody} !`);
-    send_email(newRecipient, newSubject, newBody); 
   };
   
  // });
@@ -50,105 +44,51 @@ function load_mailbox(mailbox) {
   
 
   if (mailbox == 'inbox'){
+
+    document.querySelector('#temp').innerHTML = '';
+    document.querySelector('#arch').innerHTML = '';
+
     fetch('/emails/inbox')
-    
     .then(response => response.json())
     .then(emails => {
      // Print emails
-     console.log(emails);
      console.log(emails[0]);
-     
-     
-      for ( x = 0; x < emails.length ; x++) {
-        //alert(emails[x].id);
-        const eID = new Number(emails[x].id); //get each email's unique ID
-        
-        //alert("EID is" + eID);
-        //var = emails[x].id;
-        newEmail = document.createElement('div');
-        newEmail.setAttribute("id", "newEmail" + eID );
-        newEmail.innerHTML = //if emails[x][read] == true bckgrnd color = white else grey
-        //this inner HTML shows the overview of the email, newEmail is the 'wrapper' for the full email.
-         `<p> <b>Sender: </b> ${emails[x]['sender']} </p>
-          <p> <b> Subject: </b>   ${emails[x]['subject']}</p> 
-          <p> <b> Time Sent: </b>${emails[x]['timestamp']} </p>`;
-          const thisEmailID = newEmail.id; //this makes sure the id for each div is different (they were all setting to 1)
-          //showEmail contains the email body, seperated to allow control over its display property
-          showEmail = document.createElement('div');
-          showEmail.setAttribute('id', 'showEmail' + eID);
-          showEmail.setAttribute('class', 'email'); //not needed
-          showEmail.innerHTML = `${emails[x].body}`; //gets the body of the email from the for loop .body
-          showEmail.style.display='none';//hides it by default
-          const showEmailID = showEmail.id; //gives this section an ID related to the email id
-          const archiveButton = document.createElement('button');
-          archiveButton.innerHTML = 'Archive Email';
-          archiveButton.onclick = function () {
-            fetch(`/emails/${eID}`, {
-              method: 'PUT',
-              body: JSON.stringify({
-                  archived: true
-              })
-            })
-          }
-          showEmail.append(archiveButton); //adds a button to allow archiving, TODO: make this a sep function
-       
-          newEmail.addEventListener('click', function() { //allows clicking on the email to display the body
-            if (document.querySelector(`#${showEmailID}`).style.display === 'none'){
-              document.querySelector(`#${showEmailID}`).style.display = 'block';
-              fetch(`/emails/${eID}`, { //sets 'read' to true when email is read
-                method: 'PUT',
-                body: JSON.stringify({
-                    read: true
-                })
-              })
-              
-              }
-              else {
-                document.querySelector(`#${showEmailID}`).style.display = 'none';
-              }
-          });
-         
-          document.querySelector('#show-mails').append(newEmail); 
-          newEmail.append(showEmail); //inbox has a show mails div, emails are appended to this, show email is appended to
-          //new email TODO make this one??
- 
-          } //endfor
+ for ( x = 0; x < emails.length ; x++) {
+     email = emails[x];
+ DisplayEmails(email);
+           } //endfor
       })//end 2nd .then
     }
-    
-  
-
-
+   
     if (mailbox == 'archive') {
+      document.querySelector('#temp').innerHTML = '';
+      document.querySelector('#arch').innerHTML = '';
  //stuff to do with the archive emails
- fetch('/emails/archive')
- .then(response => response.json())
- .then(emails => {
-  for ( x = 0; x < emails.length ; x++) {
-    //alert(emails[x].id);
-    const eID = new Number(emails[x].id); //get each email's unique ID
-    
-    //alert("EID is" + eID);
-    //var = emails[x].id;
-    newEmail = document.createElement('div');
-    newEmail.setAttribute("id", "newEmail" + eID );
-    newEmail.innerHTML = emails[x].body;
-    document.querySelector('#show-mails').append(newEmail);
-  }
-  // Print emails
-  console.log(emails);
-    })
-  }
 
 
+        fetch('/emails/archive')
+        .then(response => response.json())
+        .then(emails => {
+          for ( x = 0; x < emails.length ; x++) {
+            email = emails[x];
+            DisplayEmails(email);
+            //end fetch
+         }//END FOR - for each fetched email, append that to the div
+        })
+
+      }
 
     if (mailbox == 'sent') {
+      document.querySelector('#temp').innerHTML = '';
+      document.querySelector('#arch').innerHTML = '';
      //stuff to do with sent emails
-     fetch('/emails/sent')
-     .then(response => response.json())
-     .then(emails => {
-      // Print emails
-      console.log(emails); 
+       fetch('/emails/sent')
+      .then(response => response.json())
+      .then(emails => {
+        for (x = 0; x < emails.length ; x++){
+        email = emails[x];
+        DisplayEmails(email);
+        } //end for
     })
   }
 }
@@ -156,7 +96,6 @@ function load_mailbox(mailbox) {
 function debug() {
   console.log("DEBUG !");
 }
-
 
 function get_data() {
  const newRecipient =  document.querySelector('#compose-recipients').value;
@@ -185,6 +124,86 @@ function send_email (recipients, subject, body) {
   alert("A");
 }
 
+function DisplayEmails(email) {
+  CreateNewEmailSection(email);//creates newEmail div
+  document.querySelector('#temp').append(newEmail);
+  CreateShowEmailSection(email); //creates the showEmail div - displays the body of email, display is defaulted to none
+  //CreateArchiveButton(email); //makes the archive button
+  NewListener(email); //applied an event listener to the newEmail div to change showEmail div to block on click
+     document.querySelector('#temp').append(newEmail); 
+}
 
+function CreateNewEmailSection (email) {
+newEmail = document.createElement('div');
+newEmail.setAttribute("id", "newEmail" + email.id );
 
+if (email.read == true) {
+  newEmail.setAttribute('style', 'background-color: lightgray;border-style: solid;padding: 10px;margin: 10px;');
+}
+else {
+  newEmail.setAttribute('style', 'background-color: whiteborder-style: solid;padding: 10px;margin: 10px;');
+}
+newEmail.innerHTML = //this inner HTML shows the overview of the email, newEmail is the 'wrapper' for the full email.
 
+  `<p> <b>Sender: </b> ${email.sender} <b> Recipient: </b>${email.recipients} <b> Subject: </b>${email.subject}, <b> Time Sent: </b>${email.timestamp} </p>`;
+}
+
+function CreateShowEmailSection(email) { //showEmail is seperate to newEmail so I can have the email body hidden by default.
+  showEmail = document.createElement('div');
+          showEmail.setAttribute('id', 'showEmail' + email.id);
+          showEmail.setAttribute('style', 'background-color: antiquewhite')
+          showEmail.innerHTML = email.body; //gets the body of the email from the for loop .body
+          showEmail.style.display='none';//hides it by default
+          newEmail.append(showEmail);
+}
+
+function CreateArchiveButton(email) {
+  const archiveButton = document.createElement('button');
+  console.log(email.id + "WOI");
+  archiveButton.innerHTML = 'Archive Email';
+  archiveButton.onclick = function () {
+    alert(email.id);
+    fetch(`/emails/${email.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archived: true
+      })
+    })
+  }
+  showEmail.append(archiveButton);
+}
+function CreateUnArchiveButton(email) {
+  const archiveButton = document.createElement('button');
+ 
+  archiveButton.innerHTML = 'UnArchive Email';
+  archiveButton.onclick = function () {
+    alert(email.id);
+    fetch(`/emails/${email.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archived: false
+      })
+    })
+  }
+  showEmail.append(archiveButton);
+}
+
+function NewListener (email) {
+  console.log(document.querySelector(`#showEmail${email.id}`).style.display);
+  newEmail.addEventListener('click', function() { //allows clicking on the email to display the body
+    if (document.querySelector(`#showEmail${email.id}`).style.display === 'none'){
+      document.querySelector(`#showEmail${email.id}`).style.display = 'block';
+      ////////////////fetch email //////////////////////sets 'read' to true when email is read
+      fetch(`/emails/${email.id}`, { 
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true })
+      })
+      /////////////////end fetch ////////////////////
+      }
+      else {
+        document.querySelector(`#showEmail${email.id}`).style.display = 'none';
+      }
+  });
+  
+}
